fff763153d7991ab94f0bec4da982fca0784d1b1
CarlosDnl <c.ospina2001@pascualbravo.edu.co>
2018-09-30 23:06:22 -0500

    Programa que calcula los numeros perfectos desde 1 hasta 10000

---
+++ CalcularPerfe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPerfector
+{
+    class CalcularPerfe
+    {
+        public void CalPerfectos()
+        {
+            int i,j, sumar = 0;
+            for(i = 1; i < 10000; i++)
+            {
+                sumar = 0;
+                for (j = 1; j < i; j++)
+                {
+                    if (i % j == 0 && i % j != i )
+                    {
+                        sumar = sumar + j;
+                    }
+                }
+                if (sumar == i)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPerfector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcularPerfe calcular = new CalcularPerfe();
+            calcular.CalPerfectos();
+            Console.ReadKey();
+        }
+    }
+}
